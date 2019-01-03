019f334e5c05e91b735f999e341a65e524d61811
Miroslav Stoyanov <miroslav.b.stoyanov@gmail.com>
2019-01-03 20:30:47 +0200

    Add Pages for: ConfirmHide product, ConfirmShow product and AllHide products; Add hide prorerty for Product

---
+++ XeonComputers.Data/Migrations/20190103125614_ProductHide.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XeonComputers.Data.Migrations
+{
+    public partial class ProductHide : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Hide",
+                table: "Products",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hide",
+                table: "Products");
+        }
+    }
+}
---
+++ XeonComputers/Areas/Administrator/ViewModels/Products/DeleteProductViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using XeonComputers.Enums;
+
+namespace XeonComputers.Areas.Administrator.ViewModels.Products
+{
+    public class DeleteProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Име")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+
+        [Display(Name = "Спецификация")]
+        public string Specification { get; set; }
+
+        [Display(Name = "Цена")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Цена за фирми")]
+        public decimal ParnersPrice { get; set; }
+
+        [Display(Name = "Категория")]
+        public string ChildCategoryName { get; set; }
+    }
+}
